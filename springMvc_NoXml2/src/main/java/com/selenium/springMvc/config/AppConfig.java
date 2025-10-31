dcf8bf5f0231e3d63c07ac353f7e2676fb764086
Your Name <you@example.com>
2025-10-31 14:09:30 +0200

    add new spring no xml  project

---
+++ springMvc_NoXml2/src/main/java/com/selenium/springMvc/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.selenium.springMvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.selenium.springMvc")
+public class AppConfig {
+	
+	@Bean
+	InternalResourceViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+
+}
---
+++ springMvc_NoXml2/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2><br>
+
+<a href="/springMvc_NoXml/website.com/test">Welcome</a>
+</body>
+</html>
